981644d819dc097dd17e356f2a9a541c7a6a4ba6
Alexhardim <alexhardim20@gmail.com>
2024-11-30 23:29:24 +0300

    добавление: start func, utils.py

--- utils.py
+++ utils.py
@@ -1,4 +1,8 @@
 def config_par(config_path):
     with open(config_path) as config_file:
         config = dict()
-        
+        lines = config_file.readlines()
+        for line in lines:
+            k, v = line.split(' = ')
+            config[k] = v
+        return config
